8fb2412e6f61751e8c92b6eae2b69a8b1463c7ac
patrick <playbydeone@gmail.com>
2019-05-04 14:30:46 +0100

    Mise en place de l'espace utilisateur et implementation du click button connexion

---
+++ app/src/main/java/com/bydeone/vipcall/Controllers/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.bydeone.vipcall.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bydeone.vipcall.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/bydeone/vipcall/Controllers/MainActivity.java
+++ app/src/main/java/com/bydeone/vipcall/Controllers/MainActivity.java
@@ -1,15 +1,30 @@
 package com.bydeone.vipcall.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.bydeone.vipcall.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button butConnexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        butConnexion = (Button)findViewById(R.id.butConnexion);
+        butConnexion.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == butConnexion){
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        }
     }
 }
