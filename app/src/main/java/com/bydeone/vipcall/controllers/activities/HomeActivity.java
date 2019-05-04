b311023d6697b81d008d812f4cb7ac6a0c0212a0
patrick <playbydeone@gmail.com>
2019-05-04 14:56:18 +0100

    Specification d'une liste de couleur puis prise en compte des fragment dans l'espace utilisateur HomeActivity

--- app/src/main/java/com/bydeone/vipcall/controllers/activities/HomeActivity.java
+++ app/src/main/java/com/bydeone/vipcall/controllers/activities/HomeActivity.java
@@ -1,15 +1,28 @@
 package com.bydeone.vipcall.controllers.activities;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bydeone.vipcall.R;
+import com.bydeone.vipcall.models.PageAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        //3 - Configure ViewPager
+        this.configureViewPager();
+    }
+
+    private void configureViewPager(){
+        // 1 - Get ViewPager from layout
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        // 2 - Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager)) {
+        });
     }
 }
