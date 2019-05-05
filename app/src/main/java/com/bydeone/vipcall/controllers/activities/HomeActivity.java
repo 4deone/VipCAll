39205fbb68a615ebf38dc6f29f113057ac45f1d2
patrick <playbydeone@gmail.com>
2019-05-05 18:24:59 +0100

    Configure TableLayout

--- app/src/main/java/com/bydeone/vipcall/controllers/activities/HomeActivity.java
+++ app/src/main/java/com/bydeone/vipcall/controllers/activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.bydeone.vipcall.controllers.activities;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +16,10 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //3 - Configure ViewPager
-        this.configureViewPager();
+        //this.configureViewPager();
+
+        //4 - Configure TableLayout
+        this.configureViewPagerAndTabs();
     }
 
     private void configureViewPager(){
@@ -24,5 +28,19 @@ public class HomeActivity extends AppCompatActivity {
         // 2 - Set Adapter PageAdapter and glue it together
         pager.setAdapter(new PageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager)) {
         });
+    }
+
+    private void configureViewPagerAndTabs(){
+        //Get ViewPager from layout
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        //Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager)));
+
+        // 1 - Get TabLayout from layout
+        TabLayout tabs= (TabLayout)findViewById(R.id.activity_main_tabs);
+        // 2 - Glue TabLayout and ViewPager together
+        tabs.setupWithViewPager(pager);
+        // 3 - Design purpose. Tabs have the same width
+        tabs.setTabMode(TabLayout.MODE_FIXED);
     }
 }
